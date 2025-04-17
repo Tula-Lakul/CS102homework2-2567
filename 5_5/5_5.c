110859915fd835533f1f14831f9718643132d3ba
ตุลา ลากุล <tula.lak@dome.tu.ac.th>
2025-04-18 05:06:20 +0700

    Update 5_5.c

--- 5_5/5_5.c
+++ 5_5/5_5.c
@@ -12,9 +12,11 @@ int main(void) {
     }
     for(i=1 ; i<=n ; i++) {
 
-        for (j = 1;  j <= i ; j++){
-            printf("%d ",num);
-            num++;
+        for (j = 1; j < i; j++) {
+            printf(" ");
+        }
+        for (j = 1;  j <= (n - i + 1) ; j++){
+            printf("%d", j);
 
         }
         printf("\n");
